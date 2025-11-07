1f32ddfcd91d05fc2706f6239a7fe506e54a9b1f
Logrus <y-shulga@yandex.ru>
2025-11-07 15:28:53 +0300

    *

---
+++ IServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows;
+
+namespace Miracle
+{
+    namespace Service
+    {
+        public interface IServiceExtension
+        {
+            string Name { get; }
+            UIElement Content { get; }
+
+            void Load(object manager);
+
+            void Stop();
+            void OnSaveProperties();
+        }
+    }
+}
